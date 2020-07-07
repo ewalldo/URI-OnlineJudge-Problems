dfc724ab4c9bfcf2cad0d87ae64fa184f5fb6b6b
Evaldo Borba <evaldo.lborba@gmail.com>
2020-07-07 18:46:18 +0900

    Create main.py
    Solution for the problem "How Much Cassava?"

---
+++ Beginner/2936 - How Much Cassava?/main.py
@@ -0,0 +1,18 @@
+total = 225
+
+value = int(input()) * 300
+total += value
+
+value = int(input()) * 1500
+total += value
+
+value = int(input()) * 600
+total += value
+
+value = int(input()) * 1000
+total += value
+
+value = int(input()) * 150
+total += value
+
+print(total)
