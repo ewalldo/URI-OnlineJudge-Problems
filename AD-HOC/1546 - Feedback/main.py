dde67230a4f37c3ea005c60f89b8b6623e59a523
Evaldo Borba <evaldo.lborba@gmail.com>
2020-07-08 12:10:25 +0900

    Create main.py
    Solution for the problem "Feedback"

---
+++ AD-HOC/1546 - Feedback/main.py
@@ -0,0 +1,16 @@
+n_input = int(input())
+
+for i in range(n_input):
+	n_cases = int(input())
+
+	for j in range(n_cases):
+		feedback = int(input())
+
+		if feedback == 1:
+			print("Rolien")
+		elif feedback == 2:
+			print("Naej")
+		elif feedback == 3:
+			print("Elehcim")
+		elif feedback == 4:
+			print("Odranoel")
