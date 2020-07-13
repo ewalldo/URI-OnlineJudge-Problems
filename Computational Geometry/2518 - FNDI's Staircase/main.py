aeb34e56847942f130540172d418fd36008c0bd1
Evaldo Borba <evaldo.lborba@gmail.com>
2020-07-13 17:16:16 +0900

    Create main.py
    Solution for the problem "FNDI's Staircase"

--- Computational Geometry/2518 - FNDI's Staircase/main.py
+++ Computational Geometry/2518 - FNDI's Staircase/main.py
@@ -1,1 +1,17 @@
+import math
 
+while True:
+	try:
+		n_steps = int(input())
+
+		h, c, l = map(int, input().split(' '))
+
+		h /= 100
+		c /= 100
+		l /= 100
+
+		hc = math.sqrt((c**2) + (h**2))
+
+		print("%.4f" % (l * ((n_steps) * hc)))
+	except EOFError:
+		break
