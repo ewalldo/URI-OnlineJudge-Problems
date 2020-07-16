967662ecb1d649adb4f9ef3bfd8622f1cf492b6e
Evaldo Borba <evaldo.lborba@gmail.com>
2020-07-16 15:15:00 +0900

    Create main.py
    Solution for the problem "Simple Sort"

---
+++ Beginner/1042 - Simple Sort/main.py
@@ -0,0 +1,12 @@
+values = str(input()).split()
+v1, v2, v3 = int(values[0]), int(values[1]), int(values[2])
+
+x = sorted([v1, v2, v3])
+
+print(x[0])
+print(x[1])
+print(x[2])
+print("")
+print(v1)
+print(v2)
+print(v3)
