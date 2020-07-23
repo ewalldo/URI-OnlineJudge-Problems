60108ba4269fa0477165e39289c8e1a50c6ff734
Evaldo Borba <evaldo.lborba@gmail.com>
2020-07-23 18:55:35 +0900

    Create main.py
    Solution for the problem "DDD"

---
+++ Beginner/1050 - DDD/main.py
@@ -0,0 +1,23 @@
+aux = input()
+name = int(aux)
+
+#line = name.strip().split(' ')
+
+if name == 61:
+	print("Brasilia")
+elif name == 71:
+	print("Salvador")
+elif name == 11:
+	print("Sao Paulo")
+elif name == 21:
+	print("Rio de Janeiro")
+elif name == 32:
+	print("Juiz de Fora")
+elif name == 19:
+	print("Campinas")
+elif name == 27:
+	print("Vitoria")
+elif name == 31:
+	print("Belo Horizonte")
+else:
+	print("DDD nao cadastrado")
