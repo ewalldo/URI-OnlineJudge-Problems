34c772586b955fca4cb2401502939578dac7ce8f
Evaldo Borba <evaldo.lborba@gmail.com>
2020-07-26 10:44:02 +0900

    Create main.py
    Solution for the problem "Month"

---
+++ Beginner/1052 - Month/main.py
@@ -0,0 +1,27 @@
+aux = input()
+name = int(aux)
+
+if name == 1:
+	print("January")
+elif name == 2:
+	print("February")
+elif name == 3:
+	print("March")
+elif name == 4:
+	print("April")
+elif name == 5:
+	print("May")
+elif name == 6:
+	print("June")
+elif name == 7:
+	print("July")
+elif name == 8:
+	print("August")
+elif name == 9:
+	print("September")
+elif name == 10:
+	print("October")
+elif name == 11:
+	print("November")
+elif name == 12:
+	print("December")
